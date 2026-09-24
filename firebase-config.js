dc9f09256c4017eec5a0cb5990f6f7def32ffd97
stp-science <talbot.g85@gmail.com>
2026-09-24 17:45:52 +1200

    Add firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Paste the Firebase web app configuration from Firebase Console > Project settings > Your apps.
+// Firebase's web config is designed to be public; access is protected by Authentication + Firestore Rules.
+export const firebaseConfig = {
+  apiKey: "REPLACE_ME",
+  authDomain: "REPLACE_ME.firebaseapp.com",
+  projectId: "REPLACE_ME",
+  storageBucket: "REPLACE_ME.firebasestorage.app",
+  messagingSenderId: "REPLACE_ME",
+  appId: "REPLACE_ME"
+};
